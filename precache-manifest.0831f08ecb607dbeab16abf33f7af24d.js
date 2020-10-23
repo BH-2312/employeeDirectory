6db4c167d0e90d9c11b748ffa2dea1643fe238b6
Ben Hilliard <benhilliard23@hotmail.com>
2020-10-23 17:47:13 +1030

    Updates

---
+++ precache-manifest.0831f08ecb607dbeab16abf33f7af24d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c755a5f6e669599cc23c4d857c28398",
+    "url": "/employeeDirectory/index.html"
+  },
+  {
+    "revision": "7084cf7cf3049fb2235f",
+    "url": "/employeeDirectory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "94589ba8aada46a49525",
+    "url": "/employeeDirectory/static/css/main.0bef81f4.chunk.css"
+  },
+  {
+    "revision": "7084cf7cf3049fb2235f",
+    "url": "/employeeDirectory/static/js/2.f3eda909.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employeeDirectory/static/js/2.f3eda909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94589ba8aada46a49525",
+    "url": "/employeeDirectory/static/js/main.a2d1ea7d.chunk.js"
+  },
+  {
+    "revision": "417b8f3870e74cdaff5c",
+    "url": "/employeeDirectory/static/js/runtime-main.b647f509.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employeeDIrectory/precache-manifest.897c9f466c6e12692c6126d9998272f8.js"
+  "/employeeDirectory/precache-manifest.0831f08ecb607dbeab16abf33f7af24d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employeeDIrectory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employeeDirectory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
